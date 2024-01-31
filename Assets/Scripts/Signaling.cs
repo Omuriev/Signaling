9eff9e52b042c8efdfd3793153edf7b6a041eb90
Omuriev <oleguer132@gmail.com>
2024-01-31 21:46:51 +0300

    fix ChangeVolume coroutine

--- Assets/Scripts/Signaling.cs
+++ Assets/Scripts/Signaling.cs
@@ -5,39 +5,53 @@ public class Signaling : MonoBehaviour
 {
     [SerializeField] private AudioSource _audioSource;
 
-    private float _targetVolumeValue;
     private float _maxVolumeValue = 1.0f;
     private float _minVolumeValue = 0.0f;
+    private Coroutine _ChangeVolumeCoroutine;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent(out Player player))
         {
-            _audioSource.Play();
-            _targetVolumeValue = _maxVolumeValue;
-            StartCoroutine(SignalingWorker(_targetVolumeValue));
+            if (_ChangeVolumeCoroutine != null)
+            {
+                StopCoroutine(_ChangeVolumeCoroutine);
+            }
+
+            _ChangeVolumeCoroutine = StartCoroutine(ChangeVolume(_maxVolumeValue));
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        _targetVolumeValue = _minVolumeValue;
-    }
-
-    private IEnumerator SignalingWorker(float targetVolumeValue)
-    {
-        while (_audioSource.volume != targetVolumeValue)
+        if (other.TryGetComponent(out Player player))
         {
-            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, _targetVolumeValue, 0.1f * Time.deltaTime);
-
-            if (_audioSource.volume == _minVolumeValue)
+            if (_ChangeVolumeCoroutine != null)
             {
-                _audioSource.Stop();
-
-                yield break;
+                StopCoroutine(_ChangeVolumeCoroutine);
             }
 
+            _ChangeVolumeCoroutine = StartCoroutine(ChangeVolume(_minVolumeValue));
+        }
+    }
+
+    private IEnumerator ChangeVolume(float targetVolumeValue)
+    {
+        float _changeVolumeStepValue = 0.1f;
+        _audioSource.Play();
+
+        while (_audioSource.volume != targetVolumeValue)
+        {
+            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, targetVolumeValue, _changeVolumeStepValue * Time.deltaTime);
+
             yield return null;
+        }
+
+        if (_audioSource.volume == _minVolumeValue)
+        {
+            _audioSource.Stop();
+
+            yield break;
         }
     }
 }
